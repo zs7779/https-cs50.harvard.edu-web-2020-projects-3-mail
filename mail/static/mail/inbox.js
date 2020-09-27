ed692e3c347c0d9ffec020fcce3f12eb0b94601c
zs7779 <zs7779@hotmail.com>
2020-09-27 01:32:13 -0400

    add send email function

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,33 +1,67 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-  // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
-  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email);
-
-  // By default, load the inbox
-  load_mailbox('inbox');
+$(document).ready(function() {
+    // Use buttons to toggle between views
+    $('#inbox').on('click', () => load_mailbox('inbox'));
+    $('#sent').on('click', () => load_mailbox('sent'));
+    $('#archived').on('click', () => load_mailbox('archive'));
+    $('#compose').on('click', compose_email);
+    $("#id_submit_button").on('click', send_email);
+    // By default, load the inbox
+    load_mailbox('inbox');
 });
 
 function compose_email() {
+    // Show compose view and hide other views
+    $('#emails-view').hide();
+    $('#compose-view').show();
 
-  // Show compose view and hide other views
-  document.querySelector('#emails-view').style.display = 'none';
-  document.querySelector('#compose-view').style.display = 'block';
+    // Clear out composition fields
+    $('#compose-recipients').val('');
+    $('#compose-subject').val('');
+    $('#compose-body').val('');
+}
 
-  // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+function send_email() {
+    let email_req = {
+        method: 'POST',
+        body: JSON.stringify({
+            recipients: $("#compose-recipients").val(),
+            subject: $("#compose-subject").val(),
+            body: $("#compose-body").val()
+        })
+    };
+    $("#compose-recipients").removeClass("is-invalid");
+    fetch('/emails', email_req)
+    .then(response => {
+        let response_json = response.json();
+        if (!response.ok) {
+            console.log(response.status);
+            $("#compose-recipients").addClass("is-invalid");
+        }
+        return response_json
+    })
+    .then(result => {
+        console.log(result);
+        $("#id_recipients_feed_back").html(result.error);
+    });
+
+    return false;
 }
 
 function load_mailbox(mailbox) {
-  
-  // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block';
-  document.querySelector('#compose-view').style.display = 'none';
+    let mailbox_req = {
+        method: 'GET'
+    }
 
-  // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+    fetch("/emails/"+mailbox)
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+    })
+
+    // Show the mailbox and hide other views
+    $('#emails-view').show();
+    $('#compose-view').hide();
+
+    // Show the mailbox name
+    $('#emails-view').html(`<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`);
 }
